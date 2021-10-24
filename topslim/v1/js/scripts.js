d077e999109d94ab61f769bfbd39fb9c96091517
Pong Tw <pong.tw34@gmail.com>
2021-10-24 21:01:02 +0700

    product 1 cascual/jquery

--- topslim/v1/js/scripts.js
+++ topslim/v1/js/scripts.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', event => {
             return;
         }
         if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+            // navbarCollapsible.classList.remove('navbar-shrink')
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
         }
